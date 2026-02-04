a1f9268ba96f1dde6d7dcb8183451de2761f8918
kj12030540 <1johnkwon@gmail.com>
2026-02-04 17:00:57 -0600

    Clone repository

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPractice.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.util.Timer;
+
+public class pedroPractice {
+    private Follower follower;
+    private Timer pathTimer;
+    private int pathState;
+    Pose start = new Pose(56, 8, Math.toRadians(90));
+    Pose exit = new Pose(56, 30, Math.toRadians(90));
+
+    public void buildRoute(){
+
+    }
+}
